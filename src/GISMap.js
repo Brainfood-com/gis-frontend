13f32c1ae20ebf730e6211b14c1616c1d3c8e0c6
Adam Heath <doogie@brainfood.com>
2018-06-18 10:03:35 -0500

    Extract DraggableCanvasPosition.

--- src/GISMap.js
+++ src/GISMap.js
@@ -6,10 +6,9 @@ import { withStyles } from '@material-ui/core/styles'
 import { MapLayer, Popup, FeatureGroup, GeoJSON, Map, TileLayer, WMSTileLayer, LayersControl, MapControl, Circle, CircleMarker, ScaleControl, Polygon, Polyline, PropTypes as LeafletPropTypes } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
-import 'leaflet-geometryutil'
-import RotatableMarker from './RotatableMarker'
 import {picked} from './iiif/Picked'
 import ReIssueApiGeoServerLogin from './api/ReIssueApiGeoServerLogin'
+import DraggableCanvasPosition from './leaflet/DraggableCanvasPosition'
 
 import leafletMarkerIcon from 'leaflet/dist/images/marker-icon.png'
 import leafletMarkerIconRetina from 'leaflet/dist/images/marker-icon-2x.png'
@@ -348,62 +347,6 @@ class IIIFGeoJSON extends GeoJSON {
   }
 }
 
-
-class DraggableCanvasPosition extends React.Component {
-  static defaultProps = {
-    onUpdatePoint(id, point) { },
-    onCanvasSelect(id) { },
-  }
-
-  handleOnClick = event => {
-    const {onCanvasSelect, canvas} = this.props
-    onCanvasSelect(canvas.get('id'))
-  }
-
-  handleOnDragStart = (event) => {
-    console.log('dragstart')
-  }
-
-  handleOnDrag = (event) => {
-    const {allPoints} = this.props
-    //console.log('drag', event)
-    const {latlng, target} = event
-    const {_map: map} = target
-
-    const fixedLatlng = L.GeometryUtil.closest(map, allPoints, latlng)
-    target.setLatLng(fixedLatlng)
-  }
-
-  handleOnDragEnd = (event) => {
-    const {onUpdatePoint, canvas} = this.props
-    onUpdatePoint(canvas, event.target.getLatLng())
-  }
-  
-  render() {
-    const {selected, canvas, rangePoint, isFirst, isLast, zoom, fovOrientation} = this.props
-    const overrides = canvas.get('overrides')
-    const {bearing, point} = rangePoint
-
-    const overridePoint = (overrides || []).find(override => override.point)
-    const hasOverridePoint = !!overridePoint
-    const isFullOpacity = selected || isFirst || isLast || hasOverridePoint
-
-    const isHidden = zoom < 16
-    //rotationAngle={hasOverridePoint ? 180 : 0}
-    const rotationAngle = bearing + (fovOrientation === 'left' ? 90 : -90)
-    return <RotatableMarker
-      rotationAngle={rotationAngle}
-      draggable={isFullOpacity || !isHidden}
-      opacity={isFullOpacity ? 1 : isHidden ? 0 : 0.6}
-      position={point ? [point.coordinates[1], point.coordinates[0]] : null}
-      onClick={this.handleOnClick}
-      onDragstart={this.handleOnDragStart}
-      onDrag={this.handleOnDrag}
-      onDragend={this.handleOnDragEnd}
-      onViewportChange={this.onViewportChange}
-      />
-  }
-}
 
 const RangePoints = picked(['range', 'canvas'])(class RangePoints extends React.Component {
   onUpdatePoint = (canvas, point) => {
---
+++ src/leaflet/DraggableCanvasPosition.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import L from 'leaflet'
+import 'leaflet-geometryutil'
+import RotatableMarker from '../RotatableMarker'
+
+export default class DraggableCanvasPosition extends React.Component {
+  static defaultProps = {
+    onUpdatePoint(id, point) { },
+    onCanvasSelect(id) { },
+  }
+
+  handleOnClick = event => {
+    const {onCanvasSelect, canvas} = this.props
+    onCanvasSelect(canvas.get('id'))
+  }
+
+  handleOnDragStart = (event) => {
+    console.log('dragstart')
+  }
+
+  handleOnDrag = (event) => {
+    const {allPoints} = this.props
+    //console.log('drag', event)
+    const {latlng, target} = event
+    const {_map: map} = target
+
+    const fixedLatlng = L.GeometryUtil.closest(map, allPoints, latlng)
+    target.setLatLng(fixedLatlng)
+  }
+
+  handleOnDragEnd = (event) => {
+    const {onUpdatePoint, canvas} = this.props
+    onUpdatePoint(canvas, event.target.getLatLng())
+  }
+  
+  render() {
+    const {selected, canvas, rangePoint, isFirst, isLast, zoom, fovOrientation} = this.props
+    const overrides = canvas.get('overrides')
+    const {bearing, point} = rangePoint
+
+    const overridePoint = (overrides || []).find(override => override.point)
+    const hasOverridePoint = !!overridePoint
+    const isFullOpacity = selected || isFirst || isLast || hasOverridePoint
+
+    const isHidden = zoom < 16
+    //rotationAngle={hasOverridePoint ? 180 : 0}
+    const rotationAngle = bearing + (fovOrientation === 'left' ? 90 : -90)
+    return <RotatableMarker
+      rotationAngle={rotationAngle}
+      draggable={isFullOpacity || !isHidden}
+      opacity={isFullOpacity ? 1 : isHidden ? 0 : 0.6}
+      position={point ? [point.coordinates[1], point.coordinates[0]] : null}
+      onClick={this.handleOnClick}
+      onDragstart={this.handleOnDragStart}
+      onDrag={this.handleOnDrag}
+      onDragend={this.handleOnDragEnd}
+      onViewportChange={this.onViewportChange}
+      />
+  }
+}
+
