e7cbbdffd204c02a122402a5bbe40a6203f0401d
Adam Heath <doogie@brainfood.com>
2018-09-21 16:02:01 -0500

    Display taxdata for the building in a popup.

--- src/leaflet/RangePoints.js
+++ src/leaflet/RangePoints.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
-import { FeatureGroup, GeoJSON } from 'react-leaflet'
+import { Map } from 'immutable'
+import { FeatureGroup, GeoJSON, Popup } from 'react-leaflet'
 import { picked } from '../iiif/Picked'
 import DraggableCanvasPosition from './DraggableCanvasPosition'
 import GISGeoJSON from '../GISGeoJSON'
+
+const emptyMap = Map()
+
+class Building extends React.Component {
+  render() {
+    const {building} = this.props
+    if (!building) {
+      return null
+    }
+    const geojson = building.get('geojson').toJS()
+    const taxdata = building.get('taxdata', emptyMap)
+    return <GeoJSON data={geojson}>
+      <Popup>
+        <div>
+          <div>Roll Year: {taxdata && taxdata.get('roll_year')}</div>
+          <div>Total Value: {taxdata && taxdata.get('total_value')}</div>
+          <div>Net Taxable Value: {taxdata && taxdata.get('net_taxable_value')}</div>
+        </div>
+      </Popup>
+    </GeoJSON>
+  }
+}
 
 export default picked(['range', 'canvas'])(class RangePoints extends React.Component {
   onUpdatePoint = (canvas, point) => {
@@ -26,10 +49,10 @@ export default picked(['range', 'canvas'])(class RangePoints extends React.Compo
 //      { selected && buildings ? buildings.map(building => <GISGeoJSON key={building.id} data={building.geojson}/>) : null }
 
     return <FeatureGroup>
-      { camera ? <GISGeoJSON data={camera}/> : null }
+      { camera ? <GISGeoJSON pane='tilePane' data={camera}/> : null }
       { pointBuildings && pointBuildings.map(id => {
         const building = buildings.get(id)
-        return building ? <GeoJSON key={id} data={building.get('geojson').toJS()}/> : null
+        return <Building key={id} building={building}/>
       }) }
       {notNullCanvases.map((canvas, index) => {
         const id = canvas.get('id')
