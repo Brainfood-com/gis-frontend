0d6b2da9ce23fa495f18d5d9636b3a5ab749ff53
Adam Heath <doogie@brainfood.com>
2019-12-02 10:43:25 -0600

    Add support for www.* in hostnames.

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-const [full, protocol, hostBase, hostSuffix] = window.location.href.match(/(https?:\/\/)((?:.*\.)?gis(?:-app)?)(\.[^:\/]+(:\d+)?).*/)
+const [full, protocol, hostBase, hostSuffix] = window.location.href.match(/(https?:\/\/)(?:www\.)?((?:.*\.)?gis(?:-app)?)(\.[^:\/]+(:\d+)?).*/)
 
 export const makeUrl = (host, prefix) => `${protocol}${host}.${hostBase}${hostSuffix}/${prefix}`
 export const safeGetImmutableId = item => item ? item.get('id') : null
